715ac27543b9bf23c68e812cfdd26e1966012119
320014135 <pankaj.nprasad@gmail.com>
2023-08-03 15:20:07 +0530

    code refactored

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from test.test_number_pair import test_number_to_pair
+from test.test_pair_number import test_pair_to_number
+
+
+if __name__ == '__main__':
+    test_number_to_pair(4, 'White', 'Brown')
+    test_number_to_pair(5, 'White', 'Slate')
+    test_pair_to_number('Black', 'Orange', 12)
+    test_pair_to_number('Violet', 'Slate', 25)
+    test_pair_to_number('Red', 'Orange', 7)
+    print('Done :)')
--- test_pair_number.py
+++
@@ -1,6 +0,0 @@
-from main import get_pair_number_from_color
-
-
-def test_pair_to_number(major_color, minor_color, expected_pair_number):
-    pair_number = get_pair_number_from_color(major_color, minor_color)
-    assert(pair_number == expected_pair_number)
